d1a8f090e812262920c6ce1e1f9795332f149626
pojiro <pojiro.jp@gmail.com>
2020-11-24 09:04:00 +0900

    Fix bug

--- lib/live_view_upload_test_web/live/upload_component.ex
+++ lib/live_view_upload_test_web/live/upload_component.ex
@@ -3,11 +3,13 @@ defmodule LiveViewUploadTestWeb.UploadComponent do
 
   def render(assigns) do
     ~L"""
+    <div>
     <%= for entry <- @uploads.avatar.entries do %>
     <%= entry.client_name %> - <%= entry.progress %>%
     <% end %>
+    </div>
 
-    <form phx-submit="save" phx-change="validate">
+    <form phx-target="<%=@myself %>" phx-submit="save" phx-change="validate">
     <%= live_file_input @uploads.avatar %>
     <button type="submit">Upload</button>
     </form>
@@ -17,7 +19,6 @@ defmodule LiveViewUploadTestWeb.UploadComponent do
   def mount(socket) do
     {:ok,
      socket
-     |> assign(:uploaded_files, [])
      |> allow_upload(:avatar, accept: :any, max_entries: 2)}
   end
 
@@ -30,13 +31,11 @@ defmodule LiveViewUploadTestWeb.UploadComponent do
   end
 
   def handle_event("save", _params, socket) do
-    uploaded_files =
-      consume_uploaded_entries(socket, :avatar, fn %{path: path}, _entry ->
-        dest = Path.join("priv/static/uploads", Path.basename(path))
-        File.cp!(path, dest)
-        Routes.static_path(socket, "/uploads/#{Path.basename(dest)}")
-      end)
+    consume_uploaded_entries(socket, :avatar, fn %{path: path}, _entry ->
+      dest = Path.join("priv/uploads", Path.basename(path))
+      File.cp!(path, dest)
+    end)
 
-    {:noreply, update(socket, :uploaded_files, &(&1 ++ uploaded_files))}
+    {:noreply, socket}
   end
 end
